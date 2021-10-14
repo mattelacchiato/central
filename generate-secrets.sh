ebc35ebb837897da815c3696914669f28df6cb72
Matthias Brandt <matthias.brandt@inoio.de>
2021-10-14 10:07:45 +0200

    env vars

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -6,14 +6,17 @@ ROOT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "$ROOT/.env"
 
 if [ -z "$ENKETO_SECRET" ]; then
+  echo "Generating ENKETO_SECRET"
   export ENKETO_SECRET=$(LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c64)
 fi
 
 if [ -z "$ENKETO_LESS_SECRET" ]; then
+  echo "Generating ENKETO_LESS_SECRET"
   export ENKETO_LESS_SECRET=$(LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c32)
 fi
 
 if [ -z "$ENKETO_API_KEY" ]; then
+  echo "Generating ENKETO_API_KEY"
   export ENKETO_API_KEY=$(LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c128)
 fi
 
